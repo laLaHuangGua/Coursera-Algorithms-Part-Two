deafa9a30c369b41410926422f738002b031fafd
laLaHuangGua <1270562182@qq.com>
2023-04-08 19:55:50 +0800

    WordNet Attempt1: add Java file Outcast

---
+++ WordNet/attempt1/Outcast.java
@@ -0,0 +1,15 @@
+package WordNet.attempt1;
+
+public class Outcast {
+  public Outcast(WordNet wordnet) {
+  }
+
+  // given an array of WordNet nouns, return an outcast
+  public String outcast(String[] nouns) {
+    return null;
+  }
+
+  // see test client below
+  public static void main(String[] args) {
+  }
+}
